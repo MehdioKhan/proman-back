3d0dcfed2d75c0f7f1b416263ee200e886a19f10
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2019-12-24 20:16:03 +0330

    add tagMixin model

--- project/tagging/models.py
+++ project/tagging/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import gettext_lazy as _
+
+
+class TagMixin(models.Model):
+
+    tags = ArrayField(models.CharField(max_length=50),
+                      null=True,
+                      blank=True,
+                      verbose_name=_('tags'),
+                      default=list)
+
+    class Meta:
+        abstract = True
