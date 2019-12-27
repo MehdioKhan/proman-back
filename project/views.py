1e507c95840ab458e21da815c9218cc31f583245
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2019-12-27 14:31:00 +0330

    add base serializer and viewsets for project

--- project/views.py
+++ project/views.py
@@ -1,3 +1,19 @@
+from rest_framework import viewsets
+from .serializers import TaskStatusSerializer,\
+    ProjectSerializer,MembershipSerializer
+from .models import Project,TaskStatus,Membership
 
 
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
 
+
+class TaskStatusViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskStatusSerializer
+    queryset = TaskStatus.objects.all()
+
+
+class MembershipViewSet(viewsets.ModelViewSet):
+    serializer_class = MembershipSerializer
+    queryset = Membership.objects.all()
