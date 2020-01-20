a0a7b4b6b40fd5f30b96ab61ad0cfdbad7af6cc7
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-01-20 15:03:06 +0330

    add task view set

--- project/task/views.py
+++ project/task/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer,TaskListSerializer
 from .models import Task
 
 
 class TaskViewSet(viewsets.ModelViewSet):
-    serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return TaskListSerializer
+        else:
+            return TaskSerializer
