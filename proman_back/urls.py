5d4d17b0587104f4a6288880b0107770f1b6c643
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-01-20 15:48:49 +0330

    fix confilicts

--- proman_back/urls.py
+++ proman_back/urls.py
@@ -24,9 +24,5 @@ scheme_view = get_swagger_view('Proman API')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('doc/',scheme_view),
-<<<<<<< HEAD
-    path('api/v1/',include('project.urls',namespace='project')),
-=======
     path('api/v1/',include(router.urls)),
->>>>>>> khan
 ]
