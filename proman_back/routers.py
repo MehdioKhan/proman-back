dd69845658af094f5ec6339c8b6cf205e2408789
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-02-03 14:10:34 +0330

    register comment urls

--- proman_back/routers.py
+++ proman_back/routers.py
@@ -7,6 +7,7 @@ from project.attachments import views as attachment_views
 router = routers.DefaultRouter()
 
 router.register('tasks',task_views.TaskViewSet)
+router.register('comment',task_views.CommentViewSet)
 router.register('projects',project_views.ProjectViewSet)
 router.register('memberships',project_views.MembershipViewSet)
 router.register('task_statuses',project_views.TaskStatusViewSet)
