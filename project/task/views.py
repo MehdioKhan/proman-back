ddca9f225befda2c9a35d75563aa843f7b64e255
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-02-11 18:46:31 +0330

    filter comment and task with parameters

--- project/task/views.py
+++ project/task/views.py
@@ -19,7 +19,10 @@ class TaskViewSet(viewsets.ModelViewSet):
             return TaskSerializer
 
     def list(self, request, *args, **kwargs):
-        tasks = self.queryset.filter(owner=request.user)
+        project = request.GET.get('project',None)
+        if not project:
+            return response.Response({'error':'project not provided'},404)
+        tasks = self.queryset.filter(owner=request.user,project=project)
         serializer = self.get_serializer_class()(tasks,many=True)
         return response.Response(serializer.data)
 
@@ -30,4 +33,12 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated,]
     authentication_classes = [BasicAuthentication,
                               TokenAuthentication]
+
+    def list(self, request, *args, **kwargs):
+        task = request.GET.get('task',None)
+        if not task:
+            return response.Response({'error':'task_id not provided'},404)
+        comments = self.queryset.filter(user=request.user,task=task)
+        serializer = self.get_serializer_class()(comments,many=True)
+        return response.Response(serializer.data)
 
