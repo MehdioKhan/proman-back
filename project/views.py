9cbbe1d2e33bb79fcb12c56c2e7c1e872cff8dd4
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-02-11 17:06:48 +0330

    add permissions and auth classes

--- project/views.py
+++ project/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication,\
+    TokenAuthentication
 from .serializers import TaskStatusSerializer,\
     ProjectSerializer,MembershipSerializer
 from .models import Project,TaskStatus,Membership
@@ -7,13 +10,20 @@ from .models import Project,TaskStatus,Membership
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [BasicAuthentication,TokenAuthentication]
 
 
 class TaskStatusViewSet(viewsets.ModelViewSet):
     serializer_class = TaskStatusSerializer
     queryset = TaskStatus.objects.all()
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [BasicAuthentication,TokenAuthentication]
 
 
 class MembershipViewSet(viewsets.ModelViewSet):
     serializer_class = MembershipSerializer
     queryset = Membership.objects.all()
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [BasicAuthentication,TokenAuthentication]
+
