28dd82a2286fef288e2faa31e7bcf397bd03f0db
milad <miladabdollahi1397@gmail.com>
2020-02-13 00:29:37 +0330

    get list notes for request user

--- note/views.py
+++ note/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication,\
     TokenAuthentication
@@ -12,4 +12,9 @@ class NoteViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     authentication_classes = [BasicAuthentication,
                               TokenAuthentication]
+
+    def list(self, request, *args, **kwargs):
+        notes = self.queryset.filter(author=request.user)
+        serializer = self.get_serializer_class()(notes,many=True)
+        return response.Response(serializer.data)
 
