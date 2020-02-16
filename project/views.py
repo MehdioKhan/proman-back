7749e711cf4c05caa5b0fdb2f326d4f13200c62d
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-02-16 18:35:53 +0330

    filter memberships by project

--- project/views.py
+++ project/views.py
@@ -1,22 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication,\
     TokenAuthentication
-from .serializers import TaskStatusSerializer,\
-    ProjectSerializer,CreateMembershipSerializer,\
-    RetrieveMembershipSerializer
+from . import serializers
 from .models import Project,TaskStatus,Membership
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    serializer_class = ProjectSerializer
+    serializer_class = serializers.ProjectSerializer
     queryset = Project.objects.all()
     permission_classes = [IsAuthenticated,]
     authentication_classes = [BasicAuthentication,TokenAuthentication]
 
 
 class TaskStatusViewSet(viewsets.ModelViewSet):
-    serializer_class = TaskStatusSerializer
+    serializer_class = serializers.TaskStatusSerializer
     queryset = TaskStatus.objects.all()
     permission_classes = [IsAuthenticated,]
     authentication_classes = [BasicAuthentication,TokenAuthentication]
@@ -27,8 +25,18 @@ class MembershipViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated,]
     authentication_classes = [BasicAuthentication,TokenAuthentication]
 
+    def list(self, request, *args, **kwargs):
+        project = request.GET.get('project',None)
+        if not project:
+            return response.Response({'error':'project not provided'},404)
+        memberships = self.queryset.filter(project=project)
+        serializer = self.get_serializer_class()(memberships,many=True)
+        return response.Response(serializer.data)
+
     def get_serializer_class(self):
-        if self.action in ['list','retrieve']:
-            return RetrieveMembershipSerializer
+        if self.action == 'retrieve':
+            return serializers.RetrieveMembershipSerializer
+        elif self.action == 'list':
+            return serializers.ListMembershipSerializer
         else:
-            return CreateMembershipSerializer
+            return serializers.CreateMembershipSerializer
