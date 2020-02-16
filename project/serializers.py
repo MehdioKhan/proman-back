79f5a76d42821c39e3324b132275b6223a47512a
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-02-16 18:35:37 +0330

    include roles in memberships serializer

--- project/serializers.py
+++ project/serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
 from .models import Project,Membership,TaskStatus
-from account.serializers import UserSerializer
-
-
-class ProjectSerializer(serializers.ModelSerializer):
-    owner = serializers.HiddenField(
-        default=serializers.CurrentUserDefault()
-    )
-
-    class Meta:
-        model = Project
-        fields = ('id','name','description','owner','members','tags')
+from account.serializers import UserSerializer,RoleSerializer
 
 
 class CreateMembershipSerializer(serializers.ModelSerializer):
@@ -22,10 +12,19 @@ class CreateMembershipSerializer(serializers.ModelSerializer):
 
 class RetrieveMembershipSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    role = RoleSerializer(read_only=True)
 
     class Meta:
         model = Membership
         fields = ('id','user','project','role','is_admin')
+
+
+class ListMembershipSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Membership
+        fields = ('id','user','role','is_admin')
 
 
 class TaskStatusSerializer(serializers.ModelSerializer):
@@ -33,3 +32,12 @@ class TaskStatusSerializer(serializers.ModelSerializer):
         model = TaskStatus
         fields = ('id','name','color','project')
 
+
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        model = Project
+        fields = ('id','name','description','owner','members','tags')
