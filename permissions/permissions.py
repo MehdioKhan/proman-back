48d4f248abb3a38925fe4c36f9bdd007a5d709e8
Mahdi Khanmohammadi <mehdiokhan@gmail.com>
2020-02-18 19:58:31 +0330

    Add project admin and membership permissions

--- permissions/permissions.py
+++ permissions/permissions.py
@@ -44,3 +44,45 @@ class CommentPermission(permissions.BasePermission):
             return 'comment_task' in perms
         else:
             return False
+
+
+class ProjectAdminPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        memberships = obj.memberships.filter(user=request.user).first()
+        perms = []
+        if not memberships:
+            return False
+        else:
+            if memberships.is_admin:
+                return True
+            perms = memberships.role.permissions
+
+        if view.action in ['update', 'partial_update']:
+            return 'change_project' in perms
+        elif view.action == 'destroy':
+            return 'delete_project' in perms
+        else:
+            return False
+
+
+class ProjectMembershipPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        project = obj.project
+        memberships = project.memberships.filter(user=request.user).first()
+
+        perms = []
+        if not memberships:
+            return False
+        else:
+            if memberships.is_admin:
+                return True
+            perms = memberships.role.permissions
+
+        if view.action in ['create','update', 'partial_update']:
+            return 'add_member' in perms
+        elif view.action == 'destroy':
+            return 'remove_project' in perms
+        else:
+            return False
